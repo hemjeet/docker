83ac8f579bef9e2684cfb33625895d70a38f58d4
hemjeet <hemjeetsingh88@gmail.com>
2025-05-01 20:57:51 +0530

    5th

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy model and app
 COPY iris_model.joblib .
-COPY app.py .
+COPY main.py .
 
 # Expose port
 EXPOSE 8501
 
 # Run Streamlit
-CMD ["streamlit", "run", "app.py", "--server.port=8501"]
+CMD ["streamlit", "run", "main.py", "--server.port=8501"]
---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import joblib
+from sklearn.datasets import load_iris
+
+# Load model
+model = joblib.load('iris_model.joblib')
+iris = load_iris()
+
+st.title("🌼 Iris Flower Classifier (an this is cloud based)")
+st.write("Predict iris flower type based on measurements")
+
+# Input sliders
+sepal_length = st.slider("Sepal Length (cm)", 4.0, 8.0, 5.0)
+sepal_width = st.slider("Sepal Width (cm)", 2.0, 5.0, 3.0)
+petal_length = st.slider("Petal Length (cm)", 1.0, 7.0, 4.0)
+petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 1.0)
+
+# Prediction
+if st.button("Predict"):
+    prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+    st.success(f"This is an **{iris.target_names[prediction[0]]}** flower")
